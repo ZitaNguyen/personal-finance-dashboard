2f6c5e780107286570d50a6e9bb88ba618046f0a
Zita Nguyen <znguyen@getraline.com>
2025-05-13 15:52:19 +0200

    Add CSS to register page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,12 +4,24 @@ import Register from './pages/Register';
 export default function App() {
   return (
     <Router>
-      <nav>
-        <Link to="/register">Register</Link>
+      <nav className='navbar navbar-expand-lg navbar-light bg-light'>
+        <div className="container">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link to="/" className='nav-link'>Home</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/register" className='nav-link'>Register</Link>
+            </li>
+          </ul>
+        </div>
       </nav>
-      <Routes>
-        <Route path="/register" element={<Register />} />
-      </Routes>
+
+      <main className="container mt-5">
+        <Routes>
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </main>
     </Router>
   );
 }
